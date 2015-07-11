1effc984fa61cc370495653e207461be30144b9a
bsboris <bsboris@gmail.com>
2015-07-11 13:14:02 +0300

    App description added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'main#index'
-  match "/:trap", to: 'requests#create', via: :all
-  get "/:trap/requests", to: 'requests#index'
+  match "/:trap", to: 'requests#create', via: :all, as: :trap
+  get "/:trap/requests", to: 'requests#index', as: :requests
   get "/:trap/requests/events", to: 'requests#events'
   get "/:trap/requests/:id", to: 'requests#show', as: :request
 end
