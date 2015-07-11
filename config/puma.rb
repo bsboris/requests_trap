9a78b40bf38406668b9ed46d3c223c5df53f79ed
bsboris <bsboris@gmail.com>
2015-07-11 13:33:02 +0300

    Tweak production config

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
-threads threads_count, threads_count
+workers Integer(ENV['PUMA_WORKERS'] || 3)
+threads Integer(ENV['MIN_THREADS']  || 1), Integer(ENV['MAX_THREADS'] || 16)
 
 preload_app!
 
