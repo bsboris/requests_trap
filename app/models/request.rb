9d0d2aa4d7d92b182edd106720fde43c8572b0ba
bsboris <bsboris@gmail.com>
2015-07-11 13:19:35 +0300

    Final touches

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,8 +2,4 @@ class Request < ActiveRecord::Base
   serialize :data, JSON
 
   scope :all_for_trap, ->(trap) { where(trap: trap).order(created_at: :desc) }
-
-  def to_s
-    %Q(Request ##{id} for #{trap}")
-  end
 end
